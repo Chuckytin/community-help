0fff75b7738e7a354010208dcea6de0b5024e342
Chuckytin <angeltorcalcatalan@gmail.com>
2025-04-16 20:19:51 +0200

    Prueba de conexión a la BBDD y application.properties

--- backend/src/main/java/com/help/community/CommunityHelpApplication.java
+++ backend/src/main/java/com/help/community/CommunityHelpApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityHelpApplication {
-
 	public static void main(String[] args) {
+		System.setProperty("MYSQL_USER", "root");
+		System.setProperty("MYSQL_PASSWORD", "tu_password");
+
 		SpringApplication.run(CommunityHelpApplication.class, args);
 	}
-
 }
