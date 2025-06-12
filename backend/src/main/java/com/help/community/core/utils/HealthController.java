9ad43f4745add544004c23edcc337445ed18c1e0
Chuckytin <angeltorcalcatalan@gmail.com>
2025-06-12 19:36:55 +0200

    control health 5 min UptimeRobot

--- backend/src/main/java/com/help/community/core/utils/HealthController.java
+++ backend/src/main/java/com/help/community/core/utils/HealthController.java
@@ -2,13 +2,21 @@ package com.help.community.core.utils;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/control")
 public class HealthController {
 
-    @GetMapping("/api/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        response.put("version", "1.0.0");
+        return ResponseEntity.ok(response);
     }
 }
