5a6230ea54ed85a30a81be8a1fda5a9a889fe5d4
Chuckytin <angeltorcalcatalan@gmail.com>
2025-06-14 19:03:54 +0200

    otra prueba

--- frontend/src/components/CustomGrid.tsx
+++ frontend/src/components/CustomGrid.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Grid, GridProps } from '@mui/material';
 
-interface CustomGridProps extends GridProps {}
-
-const CustomGrid: React.FC<CustomGridProps> = ({ children, ...props }) => {
-  return <Grid {...props}>{children}</Grid>;
+type CustomGridProps = GridProps & {
+  children?: React.ReactNode;
 };
 
-export default CustomGrid; 
+const CustomGrid: React.FC<CustomGridProps> = ({ 
+  children, 
+  ...props 
+}) => {
+  return (
+    <Grid {...props}>
+      {children}
+    </Grid>
+  );
+};
+
+export default CustomGrid;
