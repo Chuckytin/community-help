ecbece8a624ddddb54eb8691f0b3dfcbb78ba930
Chuckytin <angeltorcalcatalan@gmail.com>
2025-06-14 18:44:01 +0200

    prueba 2

--- frontend/src/components/CustomGrid.tsx
+++ frontend/src/components/CustomGrid.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Grid, GridProps } from '@mui/material';
 
-interface CustomGridProps extends GridProps {
-  item?: boolean;
-}
+interface CustomGridProps extends GridProps {}
 
 const CustomGrid: React.FC<CustomGridProps> = ({ children, ...props }) => {
   return <Grid {...props}>{children}</Grid>;
